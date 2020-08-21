892ebbebaa51e0dcc48ec887c66eb5ccfd8d6350
NuWarWarLin <nuwarwarlin.kakehashi2@gmail.com>
2020-08-21 13:51:13 +0630

    add sign up and login rotue

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   
-  resources :users
+  root 'sessions#new'
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
 end
