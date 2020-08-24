b526bf68a7f0c3f293f72b201f4fd6dde8d738c3
NuWarWarLin <nuwarwarlin.kakehashi2@gmail.com>
2020-08-24 12:12:54 +0630

    modified post.rb file to add association related with favorite

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
     belongs_to :user
     mount_uploader :picture, PictureUploader
     validates :name, presence: true, length: { minimum: 10, maximum: 63206 }
+    has_many :favorites, dependent: :destroy
+    has_many :favorite_users, through: :favorites, source: :user
 end
