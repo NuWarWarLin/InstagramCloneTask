37a0ba2e3bfa7d70bdda46d4ea0a3c3cf022d719
NuWarWarLin <nuwarwarlin.kakehashi2@gmail.com>
2020-08-24 12:17:18 +0630

    add route for favorite

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  resources :favorites, only: [:create, :destroy]
   
 end
