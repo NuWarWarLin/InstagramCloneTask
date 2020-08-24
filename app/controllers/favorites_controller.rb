a6f2a52954766ad0d7c3931a2c560d97d7184bd4
NuWarWarLin <nuwarwarlin.kakehashi2@gmail.com>
2020-08-24 13:38:01 +0630

    functinally added create and delete for user's post favorite

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,18 @@
 class FavoritesController < ApplicationController
 
     def show
-        @favorite = current_user.favorites.find_by(blog_id: @blog.id)
+        @favorite = current_user.favorites.find_by(post_id: @post.id)
     end
+
+    def create
+        favorite = current_user.favorites.create(post_id: params[:post_id])
+        redirect_to posts_url, notice: "#{favorite.post.user.name}'s post registered as a favorite"
+    end
+
+    def destroy
+        favorite = current_user.favorites.find_by(id: params[:id]).destroy
+        redirect_to posts_url, notice: "#{favorite.post.user.name}'s post remove from favorite"
+    end
+
+    
 end
